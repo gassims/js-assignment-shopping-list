38353b8abd5a1ad3a09a079986abe0ecb1be0e38
gassims <45095831+gassims@users.noreply.github.com>
2023-02-04 14:08:53 +0300

    newEntry
    Adding a button next to each item as well as ensuring each item has the line-through effect when clicked.

--- script.js
+++ script.js
@@ -6,10 +6,35 @@ function inputLength() {
 	return input.value.length;
 }
 
-function createListElement() {
+/*function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
+	input.value = "";
+}*/
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	//Adding the delete button at the same time of the creation of the list time
+	const newBtn = document.createElement("button");
+	const txtBtn = document.createTextNode("X");
+	newBtn.appendChild(txtBtn);
+	newBtn.setAttribute("class","delete");
+	newBtn.addEventListener("click",deleteListItem => {
+		li.outerHTML = "";
+		newBtn.outerHTML ="";
+	});
+	// Adding the "done" class to the item at the same time of the creation of the list time
+	var toggleDone = () => {li.classList.add("done");
+	li.classList.toggle("done");}
+	toggleDone();
+	//Making sure that clicking the item apply the line-through affect
+	li.addEventListener("click", toggleDone => {
+		li.classList.toggle("done");
+	});
+	ul.appendChild(li);
+	// Adding the delete button right after the list
+	li.insertAdjacentElement("beforeend",newBtn);
 	input.value = "";
 }
 
